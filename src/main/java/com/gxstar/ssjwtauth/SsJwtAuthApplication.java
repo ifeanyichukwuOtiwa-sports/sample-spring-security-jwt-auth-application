d455708a29a274de2439c0a54a93d5e244266a6b
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2023-11-13 09:06:33 +0100

    update build.gradle and application properties file

---
+++ src/main/java/com/gxstar/ssjwtauth/SsJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gxstar.ssjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsJwtAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxstar/ssjwtauth/SsJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxstar.ssjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
